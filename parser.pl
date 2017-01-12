8ebefd1e4f86a5ef3f5f4306e229281baf054770
Astropeak <astropeak@gmail.com>
2017-01-12 10:23:20 +0800

    add a event, and use the low level api

--- parser.pl
+++ parser.pl
@@ -7,7 +7,53 @@ my $grammar = Marpa::R2::Scanless::G->new( { source => \$dsl } );
 my $input = sub {open my $fh, '<', $_[0] or die "Can not open file"; local $/; <$fh>;}
 ->('input-src');
 
-my $value_ref = $grammar->parse( \$input, 'My_Actions' );
+# my $value_ref = $grammar->parse( \$input, 'My_Actions' );
+
+my $slr = Marpa::R2::Scanless::R->new(
+    { grammar => $grammar, semantics_package => 'My_Actions' } );
+
+my $length = length $input;
+my $length_read    = $slr->read( \$input );
+while ($length_read < $length) {
+    for my $event ( @{ $slr->events() } ) {
+        my ($name) = @{$event};
+        print "Event $name\n";
+    }
+    $length_read = $slr->resume();
+}
+my $value_ref = $slr->value;
+
+# my $value = ${$value_ref};
+# print "value: $value\n"; 
+
+
+# my $actual_events = q{};
+
+#  READ: while (1) {
+
+#      my @actual_events = ();
+
+#    EVENT:
+#      for my $event ( @{ $slr->events() } ) {
+#          my ($name) = @{$event};
+#          push @actual_events, $name;
+#    }
+
+#      if (@actual_events) {
+#          $actual_events .= join q{ }, "Events at position $pos:", @actual_events;
+#          $actual_events .= "\n";
+#          print "event: $actual_events";
+#    }
+
+#      if ($pos < $length) {
+#          $pos = $slr->resume();
+#          next READ;
+#    }
+#      last READ;
+# } ## end READ: while (1)
+
+
+
 
 sub My_Actions::do_add {
     my ( undef, $t1, undef, $t2 ) = @_;
